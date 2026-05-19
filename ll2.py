d9d35b1bac7dfa23079cadf3327d7362f3ffbb75
VijitAgarwal <vijitagarwal123@gmail.com>
2026-05-20 02:41:10 +0530

    ll(search, count and display) + driver code

--- ll2.py
+++ ll2.py
@@ -15,6 +15,7 @@ class LL:
             new_node.next=self.head
             self.head=new_node
             print(f"inserted {element} at the beginning of linked list")
+            
         except ValueError:
             print("Invalid input type")
 
@@ -141,4 +142,97 @@ class LL:
         except ValueError:
             print("invalid input type")
 
+
+    def search(self):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+        try: 
+            key=int(input("enter the key you want to search for: "))
+            temp=self.head
+            position=1
+
+            while temp is not None:
+                if temp.data==key:
+                    print(f"{key} found at position: {position}")
+                    return
+                
+                temp=temp.next
+                position+=1
+
+            print("element not found")
+        except ValueError:
+            print("invalid input type to search for Node")
+
+    
+    def count(self):
+        count=0
+        temp=self.head
+
+        while temp is not None:
+            count+=1
+            temp=temp.next
+
+        print(f"total number of node is {count}")
+
+
+    def display(self):
+        if self.head is None:
+            print("list is empty")
+            return
+
+        temp=self.head
+        print("Linked list elements are")
+        while temp is not None:
+            print(temp.data,end="->")
+            temp=temp.next
+        print("NULL")
+
+
+## Driver  Code
+if __name__ == "__main__":
+    ll=LL()
+
+    while True:
+        print("\n\n")
+        print("Linked list menu")
+        print("1 - Insert At Beginning")
+        print("2 - Insert at End")
+        print("3 - Insert at Position")
+        print("4 - Delete at Beginning")
+        print("5 - Delete at End")
+        print("6 - Delete at Position")
+        print("7 - Search in List")
+        print("8 - Count number of Nodes")
+        print("9 - Display Linked List")
+        print("10 - Exit Menu")
+
+        try:
+            choice=int(input("Enter your Choice: "))
+
+            if choice==1:
+                ll.insert_beg()
+            elif choice==2:
+                ll.insert_end()
+            elif choice==3:
+                ll.insert_position()
+            elif choice==4:
+                ll.del_beginning()
+            elif choice==5:
+                ll.del_end()
+            elif choice==6:
+                ll.del_position()
+            elif choice==7:
+                ll.search()
+            elif choice==8:
+                ll.count()
+            elif choice==9:
+                ll.display()
+            elif choice==10:
+                print("program terminated")
+                break
+            else:
+                print("invalid choice")
         
+        except ValueError:
+            print("wrong input type for choice")

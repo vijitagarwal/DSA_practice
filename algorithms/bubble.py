5abdf4c3c51d5f5c6463e459a300642b6c2d0058
VijitAgarwal <vijitagarwal123@gmail.com>
2026-05-21 20:11:36 +0530

    learning algorithms, learnt bubbleSort

---
+++ algorithms/bubble.py
@@ -0,0 +1,29 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+
+        swapped = False
+
+        for j in range(n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+
+                # Swap
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+                swapped = True
+
+        # If no swapping happens, array is sorted
+        if not swapped:
+            break
+
+
+# Driver Code
+arr = [5, 6, 1, 3,10,20,2,-2,-4]
+
+bubble_sort(arr)
+
+print("Sorted Array:")
+print(arr)
+print(len(arr))
